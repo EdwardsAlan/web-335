962901dd8b3848d9fd07208b3fba26b3ef689738
EdwardsAlan <alan.edwards1103@gmail.com>
2019-06-23 22:38:48 -0400

    week 8

--- Week 8/Edwards_<calculator>.py
+++ Week 8/Edwards_<calculator>.py
@@ -1,3 +1,24 @@
+   
+# ============================================
+# ; Title:  edwards_calculator.py
+# ; Author: Professor Krasso
+# ; Date:  23 June 2019
+# ; Modified By: Alan Edwards
+# ; Description: Calculator functions
+# ;===========================================
+
+def add(x, y):
+    return x + y
+
+def subtract(x, y):
+    return x - y
+
+def divide(x, y):
+    return x / y
+
+print(add(1, 2))
+print(subtract(4, 1))
+print(divide(8, 2))    
 def add(x, y):
     return x + y
 
