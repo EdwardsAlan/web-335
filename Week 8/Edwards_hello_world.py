42143c861f5c6d765165f85580171b80abfba398
EdwardsAlan <alan.edwards1103@gmail.com>
2019-06-23 22:39:54 -0400

    week 8

--- Week 8/Edwards_hello_world.py
+++ Week 8/Edwards_hello_world.py
@@ -1,1 +1,10 @@
+
+# ============================================
+# ; Title:  edwards_hello_world.py
+# ; Author: Professor Krasso
+# ; Date:  23 June 2019
+# ; Modified By: Alan Edwards
+# ; Description: Hello World
+# ;===========================================
 print ("Hello World")
+
