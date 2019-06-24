9a7a069489efeee9f4afb65ca0965481e85f54f8
EdwardsAlan <alan.edwards1103@gmail.com>
2019-06-23 22:26:12 -0400

    week 8

---
+++ Week 8/Edwards_<calculator>.py
@@ -0,0 +1,12 @@
+def add(x, y):
+    return x + y
+
+def subtract(x, y):
+    return x - y
+
+def divide(x, y):
+    return x / y
+
+print(add(3, 33))
+print(subtract(50, 45))
+print(divide(9, 3))    
